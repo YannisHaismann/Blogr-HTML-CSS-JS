36b4bfbd5963d82cdbf126c53c6571f67b0a116a
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-01 20:09:43 +0200

    starting header

---
+++ js.js
@@ -0,0 +1,31 @@
+function showNavItems(title, block, invisibleLayer){
+    title.addEventListener("click", function(event){
+        event.preventDefault();
+        block.style.display = "block";
+        invisibleLayer.style.display = "block"
+    });
+}
+function unshowNavItems(block, invisibleLayer){
+        invisibleLayer.addEventListener("click", function(event){
+            block.style.display = "none";
+            invisibleLayer.style.display = "none";
+        });
+}
+const invisibleLayer = document.querySelector(".invisible-layer");
+//PRODUCT
+const productTitle = document.querySelector(".nav-header-product__title");
+const productList = document.querySelector(".nav-header-product__list");
+showNavItems(productTitle, productList, invisibleLayer);
+unshowNavItems(productList, invisibleLayer);
+
+//COMPANY
+const companyTitle = document.querySelector(".nav-header-company__title");
+const companyList = document.querySelector(".nav-header-company__list");
+showNavItems(companyTitle, companyList, invisibleLayer);
+unshowNavItems(companyList, invisibleLayer);
+
+//CONTACT
+const contactTitle = document.querySelector(".nav-header-contact__title");
+const contactList = document.querySelector(".nav-header-contact__list");
+showNavItems(contactTitle, contactList, invisibleLayer);
+unshowNavItems(contactList, invisibleLayer);
