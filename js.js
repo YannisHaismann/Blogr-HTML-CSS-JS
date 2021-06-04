86993b994837417c8273defb76f423587df5e765
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-04 21:07:23 +0200

    Animation arrows js onClick

--- js.js
+++ js.js
@@ -1,24 +1,35 @@
+//FLECHES
+const arrowProduct = document.querySelector(".arrow-product");
+const arrowCompany = document.querySelector(".arrow-company");
+const arrowContact = document.querySelector(".arrow-contact");
+
+const arrowProductMobile = document.querySelector(".arrow-product-mobile");
+const arrowCompanyMobile = document.querySelector(".arrow-company-mobile");
+const arrowContactMobile = document.querySelector(".arrow-contact-mobile");
+
 //DESKTOP
 // ->
 //PRODUCT
+
+
 const invisibleLayer = document.querySelector(".invisible-layer");
 
 const productTitle = document.querySelector(".nav-header-product__title");
 const productList = document.querySelector(".nav-header-product__list");
-showNavItems(productTitle, productList, invisibleLayer);
-unshowNavItems(productList, invisibleLayer);
+showNavItems(productTitle, productList, arrowProduct, invisibleLayer);
+unshowNavItems(productList, arrowProduct, invisibleLayer);
 
 //COMPANY
 const companyTitle = document.querySelector(".nav-header-company__title");
 const companyList = document.querySelector(".nav-header-company__list");
-showNavItems(companyTitle, companyList, invisibleLayer);
-unshowNavItems(companyList, invisibleLayer);
+showNavItems(companyTitle, companyList, arrowCompany, invisibleLayer);
+unshowNavItems(companyList, arrowCompany, invisibleLayer);
 
 //CONTACT
 const contactTitle = document.querySelector(".nav-header-contact__title");
 const contactList = document.querySelector(".nav-header-contact__list");
-showNavItems(contactTitle, contactList, invisibleLayer);
-unshowNavItems(contactList, invisibleLayer);
+showNavItems(contactTitle, contactList, arrowContact, invisibleLayer);
+unshowNavItems(contactList, arrowContact, invisibleLayer);
 
 //MOBILE
 // ->
@@ -47,39 +58,43 @@ hamburgerClose.addEventListener("click", function(e){
 //NAV MOBILE
 const productTitleMobile = document.querySelector(".js-mobile-product-title");
 const productListMobile = document.querySelector(".js-mobile-product-list");
-showNavItemsMobile(productTitleMobile, productListMobile);
+showNavItemsMobile(productTitleMobile, arrowProductMobile, productListMobile);
 
 const companyTitleMobile = document.querySelector(".js-mobile-company-title");
 const companyListMobile = document.querySelector(".js-mobile-company-list");
-showNavItemsMobile(companyTitleMobile, companyListMobile);
+showNavItemsMobile(companyTitleMobile, arrowCompanyMobile, companyListMobile);
 
 const contactTitleMobile = document.querySelector(".js-mobile-contact-title");
 const contactListMobile = document.querySelector(".js-mobile-contact-list");
-showNavItemsMobile(contactTitleMobile, contactListMobile);
+showNavItemsMobile(contactTitleMobile, arrowContactMobile, contactListMobile);
 
 //FONCTIONS
-function showNavItems(title, block, invisibleLayer){
+function showNavItems(title, block, arrow, invisibleLayer){
     title.addEventListener("click", function(event){
         event.preventDefault();
         block.style.display = "block";
-        invisibleLayer.style.display = "block"
+        invisibleLayer.style.display = "block";
+        arrow.style.transform = "rotate(180deg)";
     });
 }
-function unshowNavItems(block, invisibleLayer){
+function unshowNavItems(block, arrow, invisibleLayer){
     invisibleLayer.addEventListener("click", function(event){
         block.style.display = "none";
         invisibleLayer.style.display = "none";
+        arrow.style.transform = "rotate(0deg)";
     });
 }
 
 //MOBILE NAV FONCTIONS
-function showNavItemsMobile(title, block) {
+function showNavItemsMobile(title, arrow, block) {
     title.addEventListener("click", function (event) {
         event.preventDefault();
         if(block.style.display == "block"){
             block.style.display = "none";
+            arrow.style.transform = "rotate(0deg)"
         }else{
             block.style.display = "block";
+            arrow.style.transform = "rotate(180deg)"
         }
 
     });
